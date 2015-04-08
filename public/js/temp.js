a03f5a0ac118f0145a77ec677592fa2f812cbcaa
Rounaq <rounaqwl66@gmail.com>
2015-04-08 21:29:58 +0530

    Linear color scale for temp, Max: 50, Min: 0

--- public/js/temp.js
+++ public/js/temp.js
@@ -5,9 +5,28 @@ $(document).ready(function(){
 function showMax()
 {
 	$.get("/showData", function(data, status){
-        if(Number(data) > 40) $(".temp").css({"color":"red"});
-        else $(".temp").css({"color":"blue"});
+        //if(Number(data) > 40) $(".temp").css({"color":"red"});
+        //else $(".temp").css({"color":"blue"});
+        var low = 0,high = 50
+        var temp = Number(data);
+        var R = 0,B = 0;
+        if(temp<low) B = 255;
+        else if(temp>high) R = 255;
+        else {
+        	R = Math.round((temp-low)*255.0/(high-low));
+        	B = Math.round((high-temp)*255.0/(high-low));
+        }
+        $(".temp").css({"color":rgbToHex(R,0,B)});
         $(".temp").html(data+" °C");
         setTimeout(showMax, 300);
     });
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
