24a6389226f30100f84e4617fc124cd92a15ba46
Rounaq <rounaqwl66@gmail.com>
2015-04-15 21:29:35 +0530

    Maintenance, Help Page Accordion

---
+++ public/js/help.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#accordion").accordion({
+	    header: "h3",
+	    collapsible: true,
+	    autoHeight: false,
+	    navigation: true, 
+	    heightStyle: "content"
+	});
+});
